eba0ec79e5d9fcce4b83d207d9f630cb1d0119b8
suk-svg <suk@rhino-environmental.com>
2026-04-09 10:51:06 +0100

    Create index.js for Sports Day event management

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import SportsApp from '../components/SportsApp'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sports Day 2026 - Event Manager</title>
+        <meta name="description" content="Professional Sports Event Management System" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SportsApp />
+    </>
+  )
+}
